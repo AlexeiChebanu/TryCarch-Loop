fc7cf43658537a3985d4ec6eab46c53c3f98d613
user3107 <0alexchebanu@gmail.com>
2022-08-03 22:56:51 +0300

    initial

---
+++ LoopsANDConditions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoopsANDConditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            try
+            {
+                x = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                y = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            int sum = 0;
+            for (; x <= y; x++)
+                sum += x;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
